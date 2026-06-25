78b3bb4d2dc230be90c457ec4aed7f1fce033723
velandj <velandeivasigamani5@gmail.com>
2026-06-25 12:44:13 +0530

    Remove unused logo import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Calculator } from './Calculator';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       
         
           
   
